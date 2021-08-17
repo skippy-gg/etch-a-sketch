483c2763bbb7e0751c3fd8e211481cbef1ed07a2
skippy-gg <45381691+skippy-gg@users.noreply.github.com>
2021-08-17 15:35:09 -0400

    Implemented button functions

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+//==============================================================
+//                      DOM ELEMENTS                           |
+//==============================================================
+//Divs
 const main = document.getElementById('main');
 const grid = document.getElementById('grid');
-const buttons = document.querySelectorAll('button');
+//Buttons
+const colorBtn = document.getElementById('colorBtn');
+const rainbowBtn = document.getElementById('rainbowBtn');
+const eraseBtn = document.getElementById('eraseBtn');
+const clearBtn = document.getElementById('clearBtn');
+//Input
+const colorPicker = document.getElementById('colorPicker');
 
+//==============================================================
+//                      GRID FUNCTIONS                         |
+//==============================================================
 function generateGrid(x){
     resetGrid(x);
     grid.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
@@ -12,7 +25,6 @@ function generateGrid(x){
         grid.appendChild(gridItem);
     }
 }
-
 function resetGrid(x){
     let nodeList = grid.childNodes;
     while(nodeList.length>0){
@@ -20,6 +32,65 @@ function resetGrid(x){
     }
 }
 
-generateGrid(1);
-generateGrid(2);
+//==============================================================
+//                   INITIALIZE LISTENERS                      |
+//==============================================================
+function initEventListeners(){
+    colorBtn.addEventListener('click',draw);
+    rainbowBtn.addEventListener('click',draw);
+    eraseBtn.addEventListener('click',draw);
+    clearBtn.addEventListener('click',draw);
+}
 
+//==============================================================
+//                GENERATE RAINBOW COLORS                      |
+//==============================================================
+function rainbowColor(){
+    let rainbowR = Math.random()*255;
+    let rainbowG = Math.random()*255;
+    let rainbowB = Math.random()*255;
+    const rainbowColor = `rgb(${rainbowR}, ${rainbowG}, ${rainbowB})`; 
+    return rainbowColor
+}
+
+//==============================================================
+//                    BUTTON FUNCTIONS                         |
+//==============================================================
+function draw(e){
+    //Color button
+    if (e.target.textContent == "Color"){
+        document.querySelectorAll('.grid-cell').forEach(cell => {
+            cell.addEventListener('mouseover', event => {
+                cell.style.backgroundColor = colorPicker.value;
+            })
+        })
+    }
+    //Rainbow button 
+    else if (e.target.textContent == "Rainbow"){
+        document.querySelectorAll('.grid-cell').forEach(cell => {
+            cell.addEventListener('mouseover', event => {
+                cell.style.backgroundColor = rainbowColor();
+            })
+        })
+    }
+    //Erase button
+    else if (e.target.textContent == "Erase"){
+        document.querySelectorAll('.grid-cell').forEach(cell => {
+            cell.addEventListener('mouseover', event => {
+                cell.style.backgroundColor = "white";
+            })
+        })
+    }
+    //Clear button
+    else if (e.target.textContent == "Clear"){
+        const gridSize = prompt("Enter your desired grid size: ");
+        generateGrid(gridSize);
+    }
+}
+
+//==============================================================
+//                    START-UP FUNCTION CALLS                  |
+//==============================================================
+generateGrid(16);
+
+initEventListeners();
