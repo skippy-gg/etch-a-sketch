8b9832b4cd9f7ee900f5a315dea33d8694917a9b
skippy-gg <45381691+skippy-gg@users.noreply.github.com>
2021-08-16 23:42:27 -0400

    Added resetGrid function

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ const grid = document.getElementById('grid');
 const buttons = document.querySelectorAll('button');
 
 function generateGrid(x){
+    resetGrid(x);
     grid.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
     grid.style.gridTemplateRows =  `repeat(${x}, 1fr)`;
-    for (i = 1; i<=(x**2); i++){
-        const gridItem = document.createElement("div");
-        gridItem.className = "grid-cell grid";
+    for (i = 0; i<(x**2); i++){
+        let gridItem = document.createElement("div");
+        gridItem.className = "grid-cell";
         grid.appendChild(gridItem);
     }
 }
 
-generateGrid(16);
+function resetGrid(x){
+    let nodeList = grid.childNodes;
+    while(nodeList.length>0){
+        grid.firstChild.remove()
+    }
+}
+
+generateGrid(1);
+generateGrid(2);
+
